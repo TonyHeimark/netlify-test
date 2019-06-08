cca263983da8d5aa8aa7153dd2c369e19e294982
Tony Heimark <tjh-94@hotmail.com>
2019-06-08 15:07:30 +0200

    adding incomplete portfolio site

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,9 +6,25 @@ export default () => {
       <div className="contact-text">
         <h2>Contact Me</h2>
         <p>Phone: +47 41788033</p>
-        <p>Email: <a href="mailto:tony@tonyheimark.com">Tony@tonyheimark.com</a></p>
+        <p>Email: <a href="mailto:tjh@hotmail.com">Tjh@hotmail.com</a></p>
         <a href="https://www.linkedin.com/in/tony-j-heimark/"><i class="fab fa-linkedin"></i></a>
         <a href="https://github.com/TonyHeimark"><i class="fab fa-github-square"></i></a>
+      </div>
+      <div className="contact-form-container">
+        <form name="contact" netlify>
+          <p>
+            <label>Name <input type="text" name="name" /></label>
+          </p>
+          <p>
+            <label>Email <input type="email" name="email" /></label>
+          </p>
+          <p>
+            <label>Message <textarea type="text" name="message" /></label>
+          </p>
+          <p>
+            <button className="form-button" type="submit">Send</button>
+          </p>
+        </form>
       </div>
     </div>
   );
