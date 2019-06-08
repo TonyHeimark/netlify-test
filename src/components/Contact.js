79c68901b1e09b35eb1d13e05a254ef99637f0ef
Tony Heimark <tjh-94@hotmail.com>
2019-06-08 14:58:47 +0200

    adding incomplete portfolio site

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,12 +7,8 @@ export default () => {
         <h2>Contact Me</h2>
         <p>Phone: +47 41788033</p>
         <p>Email: <a href="mailto:tony@tonyheimark.com">Tony@tonyheimark.com</a></p>
-        <a href="#"><i class="fab fa-linkedin"></i></a>
-        <a href="#"><i class="fab fa-github-square"></i></a>
-        <a href="#"><i class="fab fa-facebook-square"></i></a>
-      </div>
-      <div className="contact-image-container">
-      <img src={require("../images/contact-me2.png")} alt="mobile version of website on a phone" />
+        <a href="https://www.linkedin.com/in/tony-j-heimark/"><i class="fab fa-linkedin"></i></a>
+        <a href="https://github.com/TonyHeimark"><i class="fab fa-github-square"></i></a>
       </div>
     </div>
   );
