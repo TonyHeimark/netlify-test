fc338fbe2c49c25459972955569d5d65b6de33ac
Tony Heimark <tjh-94@hotmail.com>
2020-09-26 12:02:59 +0200

    trigger netlify rebuild

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
-export default (props) => {
+export default props => {
   return (
     <div className="Home page">
-        <div>
-            <h1>Hi, I'm Tony</h1>
-            <p>A Front end developer based in Oslo</p>
-            <button className="cta-btn" onClick={props.handlePortfolio}>Check out my work!</button>
-        </div>
-        <div>
-            <img src={require("../images/profilbilde-min.png")} alt="of Tony Heimark" />
-        </div>
+      <div>
+        <h1>Hi, I'm Tony</h1>
+        <p>A Front end developer based in Oslo!</p>
+        <button className="cta-btn" onClick={props.handlePortfolio}>
+          Check out my work!
+        </button>
+      </div>
+      <div>
+        <img
+          src={require('../images/profilbilde-min.png')}
+          alt="of Tony Heimark"
+        />
+      </div>
     </div>
   );
-}
+};
